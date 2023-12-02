68e10788c8c9082a13d5e58d347d3e55dcc0b5be
EstebanCastel <111070716+EstebanCastel@users.noreply.github.com>
2023-12-02 15:26:00 -0500

    Tipohabitacion CRUD

---
+++ demo/src/main/java/com/example/demo/modelo/TipoHabitacion.java
@@ -0,0 +1,33 @@
+package com.example.demo.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Getter
+@Setter
+@Document(collection = "tipoHabitacion")
+public class TipoHabitacion {
+
+    @Id
+    private String id;
+
+    @Field("nombre")
+    private String nombre;
+
+    @Field("capacidad")
+    private int capacidad;
+
+    @Field("dotacion")
+    private String dotacion;
+
+    public TipoHabitacion() {}
+
+    public TipoHabitacion(String nombre, int capacidad, String dotacion) {
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.dotacion = dotacion;
+    }
+}
